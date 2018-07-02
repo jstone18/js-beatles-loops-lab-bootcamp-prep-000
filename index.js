76bfa208cfc3da2638b54635c1ce42edc6e8ae3c
Jason Stone <jason.stone0808@gmail.com>
2018-07-02 01:49:21 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+function theBeatlesPlay(musicians, instruments) {
+  const arr = [ ]
+
+  for (let i = 0, l = musicians.length; i < l; i++) {
+    arr.push(`${musicians[1]} plays ${instruments[1]}`)
+  }
+  return arr
+}
